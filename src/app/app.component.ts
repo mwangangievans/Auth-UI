3d30dcee3ca5f073563f7ccc9ea8da9788e60627
emwangangi <emwangangi@jambopay.com>
2024-05-11 21:17:36 +0300

    opt component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,15 @@ import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from "./login/login.component";
 import { ForgotPasswordComponent } from "./forgot-password/forgot-password.component";
 import { RegisterComponent } from "./register/register.component";
+import { VerifyComponent } from "./verify/verify.component";
+import { OtpComponent } from "./otp/otp.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, LoginComponent, ForgotPasswordComponent, RegisterComponent]
+    imports: [RouterOutlet, LoginComponent, ForgotPasswordComponent, RegisterComponent, VerifyComponent, OtpComponent]
 })
 export class AppComponent {
   title = 'AUTH';
