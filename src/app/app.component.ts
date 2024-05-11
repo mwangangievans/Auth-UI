01f0364280664e43193521409acc691ae876051d
emwangangi <emwangangi@jambopay.com>
2024-05-11 20:14:08 +0300

    login and forget password page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from "./login/login.component";
+import { ForgotPasswordComponent } from "./forgot-password/forgot-password.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, LoginComponent, ForgotPasswordComponent]
 })
 export class AppComponent {
   title = 'AUTH';
---
+++ tailwind.config.js
@@ -0,0 +1,60 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html, ts}",
+    "./node_modules/flowbite/**/*.js"
+  ],
+  theme: {
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
+    extend: {
+      // typograpy: (theme) => ({
+      //   default:{
+      //     css: { 
+      //       a: {
+      //         textDecoration : 'none' ,
+      //         color: theme('colors.blue.500'),
+      //         fontWeight: '600',
+      //         '&:hover': {
+      //           textDecoration: 'underline',
+      //         },
+      //       }
+      //     }
+      //   }
+      // }),
+      colors: {
+        //#14B04C
+        'primary_color': '#14b04c',
+        'linkColor': '#0057e3',
+        'lightGreen': '#ecfdf3',
+        
+      },
+      fontWeight: {
+        'amy-extra-bold': 900
+      },
+      backgroundImage: {
+       
+      }
+    },
+  },
+  plugins: [
+    require('flowbite/plugin'),
+        require("daisyui"),
+    ]
+
+   
+}
