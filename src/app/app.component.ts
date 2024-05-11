fba433fa70b28d0bd29bda67a8bf5f542b124d94
emwangangi <emwangangi@jambopay.com>
2024-05-11 20:35:10 +0300

    auth pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from "./login/login.component";
 import { ForgotPasswordComponent } from "./forgot-password/forgot-password.component";
+import { RegisterComponent } from "./register/register.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, LoginComponent, ForgotPasswordComponent]
+    imports: [RouterOutlet, LoginComponent, ForgotPasswordComponent, RegisterComponent]
 })
 export class AppComponent {
   title = 'AUTH';
